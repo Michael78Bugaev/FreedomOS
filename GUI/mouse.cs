db230d7a8b3845bd86152ada01ff03ceeb2ecd34
miha <miha@WIN-RDO39NQ88QQ>
2023-11-16 20:39:36 +0300

    Добавьте файлы проекта.

---
+++ GUI/mouse.cs
@@ -0,0 +1,30 @@
+﻿using Cosmos.System;
+using Cosmos.System.Graphics;
+using IL2CPU.API.Attribs;
+using Sys = Cosmos.System;
+
+namespace guos.GUI
+{
+    public class mouse
+    {
+        private readonly Canvas _canvas;
+        private readonly Pen pen;
+        private Bitmap cursor;
+        [ManifestResourceStream(ResourceName = "guos.res.cursor.bmp")] public static byte[] curs;
+        public mouse(Canvas canvas)
+        {
+            _canvas = canvas;
+            cursor = new Bitmap(curs);
+        }
+        public void DrawCursor()
+        {
+            MouseManager.ScreenWidth = (uint)_canvas.Mode.Columns;
+            MouseManager.ScreenHeight = (uint)_canvas.Mode.Rows;
+
+            int X = (int)MouseManager.X;
+            int Y = (int)MouseManager.Y;
+
+            _canvas.DrawImageAlpha(cursor, X, Y);
+        }
+    }
+}
---
+++ Kernel.cs
@@ -0,0 +1,25 @@
+﻿using Cosmos.System.Graphics;
+using guos.GUI;
+using System.Drawing;
+using Sys = Cosmos.System;
+
+namespace guos
+{
+    public class Kernel : Sys.Kernel
+    {
+        private static Canvas canvas;
+        private mouse Mouse;
+        protected override void BeforeRun()
+        {
+            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(800, 600, ColorDepth.ColorDepth32));
+            Mouse = new mouse(canvas);
+        }
+
+        protected override void Run()
+        {
+            canvas.Clear(Color.White);
+            Mouse.DrawCursor();
+            canvas.Display();
+        }
+    }
+}
